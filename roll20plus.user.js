10915e07614831661094279059f9f69a1d0f54a4
kcaf <kcaf@users.noreply.github.com>
2016-10-13 19:36:51 -0500

    Update roll20plus.user.js

--- roll20plus.user.js
+++ roll20plus.user.js
@@ -41,7 +41,7 @@ var Roll20Plus = function() {
             var val = $.trim($(this).text());
             $(this).html("<input type='text' value='" + val + "'/>");
             $(this).find("input").focus().select();
-        }),
+        });
         $("#initiativewindow").on("keydown", ".hp.editable", function(event) {
             if (event.which == 13) {
                 var total = 0, token, id, char, hp,
@@ -53,7 +53,7 @@ var Roll20Plus = function() {
                 token = $(this).parents("li.token");
                 id = token.data("tokenid");
                 char = d20.Campaign.pages.get(d20.Campaign.activePage()).thegraphics.get(id).character;
-                hp = char.attribs.find(function(a){return a.get("name").toLowerCase() === "hp"});
+                hp = char.attribs.find(function(a){return a.get("name").toLowerCase() === "hp";});
                 if(hp){
                     hp.syncedSave({
                         current: total
@@ -106,7 +106,7 @@ var Roll20Plus = function() {
 
     /*  */
     d20plus.cssRules = [
-        {s: "#initiativewindow ul li span.initiative,#initiativewindow ul li span.ac,#initiativewindow ul li span.hp,#initiativewindow ul li span.pp", 
+        {s: "#initiativewindow ul li span.initiative,#initiativewindow ul li span.ac,#initiativewindow ul li span.hp,#initiativewindow ul li span.pp",
             r: "font-size: 25px;font-weight: bold;text-align: right;float: right;padding: 5px;width: 10%;min-height: 20px;"},
         {s: "#initiativewindow ul li span.editable input",
             r: "width: 100%; box-sizing: border-box;height: 100%;"},
@@ -130,7 +130,7 @@ var Roll20Plus = function() {
       <$ this.character = d20.Campaign.pages.get(d20.Campaign.activePage()).thegraphics.get(this.id).character; $>
       <span class='pp' alt='Passive Perception' title='Passive Perception'><$!this.character.autoCalcFormula('(10+@{perception_bonus}+@{passiveperceptionmod})')$></span>
       <span class='ac' alt='AC' title='AC'><$!this.character.autoCalcFormula('@{ac}')$></span>
-      <span class='hp editable' alt='HP' title='HP'><$!this.character.autoCalcFormula('@{hp}')$></span> 
+      <span class='hp editable' alt='HP' title='HP'><$!this.character.autoCalcFormula('@{hp}')$></span>
       <$ if(this.avatar) { $><img src='<$!this.avatar$>' /><$ } $>
       <span class='name'><$!this.name$></span>
       <div class='clear' style='height: 0px;'></div>
